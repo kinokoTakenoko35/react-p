f6c06cf7a60863f0a6e5a83523a34f7ac489a24c
kinokoTakenoko35 <kinoko35@me.com>
2020-11-20 22:47:07 +0900

    7. react router dom
     react router domを使用して、各リンク先のルートを設定。
     - index.jsでAppをRouter囲む
     - App.jsでrender する component を Route で囲んでPathを適宜する
     - Nav.js には Link でそれぞれのリンク先をTOで指定する
     -

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+//Each Styles
+import Nav from "./components/Nav";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
-import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      {/* Switchで囲むことでその中で設定したRouterのPathは最初に一致したコンポーネントだけルーティングする事ができる */}
+      <Switch>
+        {/* exactを付けることでパスが完全一致している場合表示させることができる */}
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+//router-dom の Link を使用することでリンクを簡単に作成できる
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a id="Logo" href="#">
+        <Link id="Logo" to="/">
           Capture
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href="#">1. About Us</a>
+          <Link to="/">1. About Us</Link>
         </li>
         <li>
-          <a href="#">2. Our Work</a>
+          <Link to="/work">2. Our Work</Link>
         </li>
         <li>
-          <a href="#">3. Contact Us</a>
+          <Link to="/contact">3. Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
