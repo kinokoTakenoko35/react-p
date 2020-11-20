20c08c5520e6c28164f659768d0489a1c0fb8ef4
kinokoTakenoko35 <kinoko35@me.com>
2020-11-20 16:46:55 +0900

    use Styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,67 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//Styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. we
           have professional with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="guy with home" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+//Styled Components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  /* 子要素にスタイルを適応させる場合は、親要素の中に記述する */
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
