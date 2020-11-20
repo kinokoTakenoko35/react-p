9bd57a5f8eca81334343ea0e4be232ac2be10995
kinokoTakenoko35 <kinoko35@me.com>
2020-11-20 15:45:07 +0900

    4. Faq section
    page contents done??

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,13 +2,15 @@ import React from "react";
 //Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <AboutSection />
       <ServicesSection />
-    </div>
+      <FaqSection />
+    </>
   );
 };
 
